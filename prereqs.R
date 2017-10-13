37ef4270276a5310986b8f4653f5ad516db16e1e
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-10-13 12:40:43 -0700

    data package install done only when needed

--- prereqs.R
+++ prereqs.R
@@ -1,3 +1,5 @@
+printf <- function(...) print(noquote(sprintf(...)))
+
 source("http://bioconductor.org/biocLite.R")
 
 code.pkgs <- c("GenomicRanges",
@@ -17,6 +19,8 @@ code.pkgs <- c("GenomicRanges",
                "annotate",
                "splitstackshape")
 
+biocLite(code.pkgs)
+
 data.pkgs <- c("BSgenome",
                "BSgenome.Hsapiens.UCSC.hg38",
                "BSgenome.Hsapiens.UCSC.hg19",
@@ -25,6 +29,13 @@ data.pkgs <- c("BSgenome",
                "org.Mm.eg.db",
                "SNPlocs.Hsapiens.dbSNP150.GRCh38")
 
-biocLite(c(code.pkgs, data.pkgs))
+for(data.pkg in data.pkgs){
+   suppressWarnings(
+      needed <- !require(data.pkg, character.only=TRUE, quiet=TRUE)
+      )
+   printf("%s needed? %s", data.pkg, needed)
+   if(needed)
+      biocLite(data.pkg,quiet=FALSE)
+   } # for
 
 
